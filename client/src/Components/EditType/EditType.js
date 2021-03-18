8bda3f29868a9823d5b586e56e30aad475ab0ec4
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-03-18 15:07:59 -0400

    added smoother front end for edit type section

--- client/src/Components/EditType/EditType.js
+++ client/src/Components/EditType/EditType.js
@@ -8,20 +8,108 @@ class EditType extends React.Component {
         this.state = {
             changeSuccess: false,
             deleteSuccess: false,
+            addSuccess: false,
+            typeDisplay: 'addTypeNav'
         }
-        this.myRef = React.createRef();
+        this.addType = React.createRef();
+        this.changeType = React.createRef();
+        this.changeTypeEdit = this.changeTypeEdit.bind(this);
+        this.displayTypeEdit = this.displayTypeEdit.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.submitDelete = this.submitDelete.bind(this);
         this.submitChange = this.submitChange.bind(this);
+        this.submitAdd = this.submitAdd.bind(this);
+    }
+
+    changeTypeEdit(e) {
+        const value = e.target.id;
+        this.setState({
+            typeDisplay: value,
+            displaySelected: value
+        });
+    }
+
+    displayTypeEdit(value) {
+        switch (value) {
+            case 'addTypeNav':
+                return (
+                    <div>
+                        <form id="addTypeForm" autoComplete="off" onSubmit={this.submitAdd}>
+                            <u><h2>Add Type</h2></u>
+                            <label htmlFor="addType">Type to Add</label>
+                            <input
+                                type="text"
+                                id="addType"
+                                name="addType"
+                                ref={this.addType}
+                                onChange={this.handleChange}
+                                required
+                            />
+                            <button type="submit">Add</button>
+                        </form>
+                        {this.state.addSuccess &&
+                            <h3 className="successMessage">{this.state.typeToAdd} has been added</h3>
+                        }
+                    </div>
+                );
+            case 'changeTypeNav':
+                return (
+                    <div>
+                        <form id="changeTypeForm" autoComplete="off" onSubmit={this.submitChange} >
+                            <u><h2>Change Type Name</h2></u>
+                            <label htmlFor="deleteType">Type to Change</label>
+                            <select id="changeType" onChange={this.handleChange} required>
+                                <option value=""></option>
+                                {this.props.types}
+                            </select>
+                            <label htmlFor="newTypeValue">Change to</label>
+                            <input
+                                type="text"
+                                id="newTypeValue"
+                                name="newTypeValue"
+                                ref={this.changeType}
+                                onChange={this.handleChange}
+                                required
+                            />
+                            <button type="submit">Change</button>
+                        </form>
+                        {this.state.changeSuccess &&
+                            <h3 className="successMessage">{this.state.typeToChange} is now {this.state.changeTo}</h3>
+                        }
+                    </div>
+                );
+            case 'deleteTypeNav':
+                return (
+                    <div>
+                        <form id="deleteTypeForm" onSubmit={this.submitDelete}>
+                            <u><h2>Delete Type</h2></u>
+                            <label htmlFor="deleteType">Type to Delete</label>
+                            <select id="deleteType" onChange={this.handleChange} required>
+                                <option value=""></option>
+                                {this.props.types}
+                            </select>
+                            <button type="submit">Delete</button>
+                        </form>
+                        {this.state.deleteSuccess &&
+                            <h3 className="successMessage">{this.state.typeToRemove} has been deleted</h3>
+                        }
+                    </div>
+                );
+            default:
+                console.error('Error at changeTypeEdit switch');
+        }
     }
 
     handleChange(e) {
+        //set correct state for specific change
         const typeValue = e.target.value;
         const inputId = e.target.id;
         if (inputId === 'deleteType') {
             this.setState({ typeToRemove: typeValue });
         } else if (inputId === 'changeType') {
             this.setState({ typeToChange: typeValue });
+        } else if (inputId === 'addType') {
+            this.setState({ typeToAdd: typeValue });
         } else {
             this.setState({ changeTo: typeValue });
         }
@@ -55,42 +143,37 @@ class EditType extends React.Component {
                 if (response.status === 200) {
                     this.setState({ changeSuccess: true });
                     this.props.refreshTypes();
-                    const newTypeInput = this.myRef.current;
+                    const newTypeInput = this.changeType.current;
                     newTypeInput.value = '';
                 }
             });
     }
 
+    submitAdd(e) {
+        e.preventDefault();
+        axios.post('/api/types', {
+            data: this.state.typeToAdd
+        })
+            .then(response => {
+                if (response.status === 200) {
+                    this.setState({ addSuccess: true })
+                    this.props.refreshTypes();
+                    const newType = this.addType.current;
+                    newType.value = '';
+                }
+            })
+
+    }
+
     render() {
         return (
             <div className="typeWrapper" >
-                <form id="changeTypeForm" autoComplete="off" onSubmit={this.submitChange} >
-                    <u><h2>Change Type Name</h2></u>
-                    <label htmlFor="deleteType">Select Type to Change</label>
-                    <select id="changeType" onChange={this.handleChange} required>
-                        <option value=""></option>
-                        {this.props.types}
-                    </select>
-                    <label htmlFor="newTypeValue">Change to</label>
-                    <input type="text" id="newTypeValue" name="newTypeValue" ref={this.myRef} onChange={this.handleChange} required />
-                    <button type="submit">Change</button>
-                </form>
-                {this.state.changeSuccess &&
-                    <h3 className="successMessage">{this.state.typeToChange} is now {this.state.changeTo}</h3>
-                }
-                <h1>OR</h1>
-                <form id="deleteTypeForm" onSubmit={this.submitDelete}>
-                    <u><h2>Delete Type</h2></u>
-                    <label htmlFor="deleteType">Select Type to Delete</label>
-                    <select id="deleteType" onChange={this.handleChange} required>
-                        <option value=""></option>
-                        {this.props.types}
-                    </select>
-                    <button type="submit">Delete</button>
-                </form>
-                {this.state.deleteSuccess &&
-                    <h3 className="successMessage">{this.state.typeToRemove} has been deleted</h3>
-                }
+                <ul id="editTypeNav">
+                    <li id="addTypeNav" onClick={this.changeTypeEdit}>Add Type</li>
+                    <li id="changeTypeNav" onClick={this.changeTypeEdit}>Change Type</li>
+                    <li id="deleteTypeNav" onClick={this.changeTypeEdit}>Delete Type</li>
+                </ul>
+                {this.displayTypeEdit(this.state.typeDisplay)}
             </div>
         );
     }
